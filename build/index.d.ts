add892dc666ac158719a4a087aec8db39508249e
Eric Mackrodt <ericmackrodt@gmail.com>
2019-03-14 11:37:18 +1100

    Remove build folder

--- build/index.d.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-declare function styledNative<TProps, C extends React.ComponentType<TProps>>(WrappedComponent: C): (literals: TemplateStringsArray, ...placeholders: string[]) => import("styled-components").StyledComponent<C, any, {} & TProps, keyof TProps>;
-export default styledNative;
